5d5f15a237ab922ff2853faceb412321d6506fce
Hazel Guzzetti <viizionprime@gmail.com>
2023-08-10 19:02:41 -0400

    Time: 97 ms (91.91%), Space: 19.5 MB (85.70%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = {}
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            if sorted_word in res:
+                res[sorted_word].append(word)
+            else:
+                res[sorted_word] = [word]
+        return list(res.values())
